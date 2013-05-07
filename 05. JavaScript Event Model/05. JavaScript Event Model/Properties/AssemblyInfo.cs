37e3a7f9fda90dbd4fc31bbde41ed3f28e9463a8
Yumer Yumer <y.yumer@gmail.com>
2013-05-07 12:18:15 +0300

    05. JavaScript Event Model:   01. Solved

--- 05. JavaScript Event Model/05. JavaScript Event Model/Properties/AssemblyInfo.cs
+++ 05. JavaScript Event Model/05. JavaScript Event Model/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_02.Advanced_Funcitons")]
+[assembly: AssemblyTitle("_05.JavaScript_Event_Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("_02.Advanced_Funcitons")]
+[assembly: AssemblyProduct("_05.JavaScript_Event_Model")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("68ee677b-7e0a-482b-ae39-60addfc3c6ca")]
+[assembly: Guid("a6ad3653-d8fd-45c0-b0ca-5628562411e6")]
 
 // Version information for an assembly consists of the following four values:
 //
